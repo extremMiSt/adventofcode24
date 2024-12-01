bce77e30119314846c150d7bb233ad57393125b0
Michael Steinle <michael.steinle@googlemail.com>
2024-12-01 10:47:01 +0100

    day 1 done

---
+++ day01/app/Main.hs
@@ -0,0 +1,27 @@
+module Main where
+import qualified Data.List as List
+
+split :: String -> (String, String)
+split s = (take 5 s, drop 8 s)
+
+count :: [Integer] -> Integer -> Integer
+count xs find = fromIntegral $ length (filter (== find) xs)
+
+first :: (Ord a, Read a) => [String] -> [a]
+first l = List.sort (map (read.fst.split) l)
+
+second :: (Ord a, Read a) => [String] -> [a]
+second l = List.sort (map (read.snd.split) l)
+
+task1 :: (Num a, Ord a, Read a) => [String] -> a
+task1 l = sum $ map abs $ zipWith (-) (first l) (second l)
+
+task2 :: [String] -> Integer
+task2 l = sum $ map (\x -> x * count (second l) x) (first l)
+
+main :: IO ()
+main = do
+    f <- readFile "./input01.txt"
+    print $ task1 $ lines f
+    print $ task2 $ lines f
+
