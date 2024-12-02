513f0cb288bfa8082f85d1b3e66351a65a4e9045
Michael Steinle <michael.steinle@googlemail.com>
2024-12-02 21:28:41 +0100

    some cleanup and genericing

--- day01/app/Main.hs
+++ day01/app/Main.hs
@@ -1,10 +1,10 @@
 module Main where
 import qualified Data.List as List
 
-split :: String -> (String, String)
+split :: [a] -> ([a], [a])
 split s = (take 5 s, drop 8 s)
 
-count :: [Integer] -> Integer -> Integer
+count :: (Num b, Eq a) => [a] -> a -> b
 count xs find = fromIntegral $ length (filter (== find) xs)
 
 first :: (Ord a, Read a) => [String] -> [a]
@@ -16,7 +16,7 @@ second l = List.sort (map (read.snd.split) l)
 task1 :: (Num a, Ord a, Read a) => [String] -> a
 task1 l = sum $ map abs $ zipWith (-) (first l) (second l)
 
-task2 :: [String] -> Integer
+task2 :: (Num a, Ord a, Read a) => [String] -> a
 task2 l = sum $ map (\x -> x * count (second l) x) (first l)
 
 main :: IO ()
